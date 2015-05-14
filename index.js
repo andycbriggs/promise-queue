54e09c22a4a119aa104672a4d69cde818d29545e
Andy Briggs <andycbriggs@gmail.com>
2015-05-14 10:29:19 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var PromiseQueue = function () {
+  this._queue = [];
+};
+
+PromiseQueue.prototype.queue = function (task) {
+  var self = this;
+  this._queue.push(function () {
+    task().finally(function () {
+      // this task just finished so remove it
+      self._queue.shift();
+      // and run the next one
+      self.next();
+    });
+  });
+  if (this._queue.length === 1) {
+    // this was the first item so run it
+    this.next();
+  }
+};
+
+PromiseQueue.prototype.next = function () {
+  // are there any tasks to run
+  if (this._queue.length > 0) {
+    // start the next item in the queue
+    this._queue[0]();
+  }
+};
+
+module.exports = PromiseQueue;
